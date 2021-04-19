ba815e0041c3aee1f339afd4017e455289a18134
Ravskij <79273681+Ravskij@users.noreply.github.com>
2021-04-19 20:23:55 +0300

    красивейше и больше комментариев

--- src/com/ravskij/Main.java
+++ src/com/ravskij/Main.java
@@ -3,7 +3,7 @@ package com.ravskij;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         //Класс коллекции
         ShopCollection shopCollection = new ShopCollection();
@@ -14,13 +14,12 @@ public class Main {
             System.out.println("""
                     Меню:
                     0. Закрыть программу
-                    1. Открыть файл
-                    2. Сохранить файл
-                    3. Ввести данные
-                    4. Вывести данные
-                    5. Добавить данные в конец
-                    6. Добавить данные в указанную позицию
-                    7. Удаление данных""");
+                    1. Добавить данные в конец
+                    2. Добавить данные в указанную позицию
+                    3. Удаление данных
+                    4. Открыть файл
+                    5. Сохранить файл
+                    6. Ввести данные""");
 
 
             Scanner console = new Scanner(System.in);
@@ -29,15 +28,57 @@ public class Main {
             //    String text = "0 ручка 23/05/2021";
             Shop[] p = new Shop[1];
             switch (number){
+                //Добавляет данные в конец списка
                 case 1:
+                    //Класс для добавления наименования и даты
+                    Scanner addprodname1 = new Scanner(System.in);
+                    Scanner adddate1 = new Scanner(System.in);
+                    System.out.println("Введите наименование: ");
+                    String productname1 = addprodname1.nextLine();
+                    System.out.println("Введите дату продажи: ");
+                    String date1 = adddate1.nextLine();
+                    shopCollection.add(new Shop(productname1, date1));
+                //    shopCollection.print();
+                    break;
+                //Добавляет данные в указанную позицию
+                case 2:
+                    //Класс для добавления наименования, даты и индекса
+                    Scanner addprodname2 = new Scanner(System.in);
+                    Scanner adddate2 = new Scanner(System.in);
+                    Scanner addindex2 = new Scanner(System.in);
+                    System.out.println("Введите куда добавить запись: ");
+                    String index2str = addindex2.nextLine();
+                    //Преобразование строки к int
+                    int index2int = Integer.parseInt(index2str) - 1;
+                    System.out.println("Введите наименование: ");
+                    String productname2 = addprodname2.nextLine();
+                    System.out.println("Введите дату продажи: ");
+                    String date2 = adddate2.nextLine();
+                    shopCollection.addin(index2int, new Shop(productname2, date2));
+                //    shopCollection.print();
+                    break;
+                //Удаляет данные из указанной позиции
+                case 3:
+                    //Класс для добавления индекса
+                    Scanner addindex3 = new Scanner(System.in);
+                    System.out.println("Введите какую запись удалить: ");
+                    String index3str = addindex3.nextLine();
+                    //Преобразование строки к int
+                    int index3int = Integer.parseInt(index3str) - 1;
+                    shopCollection.remove(index3int);
+                //    shopCollection.print();
+                    break;
+                //Не работает
+                case 4:
                     System.out.println("Введите название файла: ");
                     Scanner openname = new Scanner(System.in);
                     String opentofile = openname.nextLine();
                     OpenFile open = new OpenFile(opentofile);
-                //    text = open.Open();
+                    //    text = open.Open();
                     //    System.out.println(opentofile);
                     break;
-                case 2:
+                //Не работает
+                case 5:
                     System.out.println("Введите название файла: ");
                     Scanner savename = new Scanner(System.in);
                     String savetofile = savename.nextLine();
@@ -45,73 +86,8 @@ public class Main {
                     save.Save();
                     //    System.out.println(savetofile);
                     break;
-                case 3:
-                    int count = 0;
-                //    p[count] = new Shop();
-                    System.out.println("Введите наменование продукта: ");
-                    Scanner nameofproduct = new Scanner(System.in);
-                    String prodname = nameofproduct.nextLine();
-                    p[count].setProductname(prodname);
-                    System.out.println("Введите дату продажи: ");
-                    Scanner dateofsale = new Scanner(System.in);
-                    String daysale = dateofsale.nextLine();
-                    p[count].setDate(daysale);
-                    text = count + " " + prodname + " " + daysale;
-                    System.out.println(text);
-                    //    System.out.println(count);
-                    //    count++;
-
-                    for (Shop i : p) {
-                        System.out.println(i.getProductname() + " день продажи " + i.getDate());
-                    }
-                    break;
-                case 4:
-                    for (Shop i : p) {
-                        System.out.println(i.getProductname() + " день продажи " + i.getDate());
-                    }
-                    break;
-                case 5:
-                    //Класс для добавления наименования и даты
-                    Scanner addprodname5 = new Scanner(System.in);
-                    Scanner adddate5 = new Scanner(System.in);
-                    System.out.println("Введите наименование: ");
-                    String productname5 = addprodname5.nextLine();
-                    System.out.println("Введите дату продажи: ");
-                    String date5 = adddate5.nextLine();
-                    shopCollection.add(new Shop(productname5, date5));
-                    shopCollection.print();
-                    break;
+                //Вывод данных
                 case 6:
-                    //Класс для добавления наименования, даты и индекса
-                    Scanner addprodname6 = new Scanner(System.in);
-                    Scanner adddate6 = new Scanner(System.in);
-                    Scanner addindex6 = new Scanner(System.in);
-                    System.out.println("Введите куда добавить запись: ");
-                    String index6str = addindex6.nextLine();
-                    //Преобразование строки к int
-                    int index6int = Integer.parseInt(index6str) - 1;
-                    System.out.println("Введите наименование: ");
-                    String productname6 = addprodname6.nextLine();
-                    System.out.println("Введите дату продажи: ");
-                    String date6 = adddate6.nextLine();
-                    shopCollection.addin(index6int, new Shop(productname6, date6));
-                    shopCollection.print();
-                    break;
-                case 7:
-                    //Класс для добавления индекса
-                    Scanner addindex7 = new Scanner(System.in);
-                    System.out.println("Введите какую запись удалить: ");
-                    String index7str = addindex7.nextLine();
-                    //Преобразование строки к int
-                    int index7int = Integer.parseInt(index7str) - 1;
-                    shopCollection.remove(index7int);
-                    shopCollection.print();
-                    break;
-                case 8:
-                    //    Arrays.sort(p, new SortByDate());
-                    //    for(Shop i : p){
-                    //        System.out.println(i.getProductname() + " " + i.getDate());
-                    //    }
                     shopCollection.print();
                     break;
                 default:
